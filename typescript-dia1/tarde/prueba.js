f575dfe135ea6717ec341d6365a1975c3a427f6e
PIPO <josejavierperera@gmail.com>
2025-05-15 11:06:39 +0200

    Delete typescript-dia1 directory

--- typescript-dia1/tarde/prueba.js
+++
@@ -1,8 +0,0 @@
-// Pregunta al usuario por su nombre
-var nombre = prompt("¿Cuál es tu nombre?");
-if (nombre !== null) {
-    console.log("Hola, ".concat(nombre, "!"));
-}
-else {
-    console.log("No se ingresó ningún nombre.");
-}
--- typescript-dia1/tarde/prueba.ts
+++
@@ -1,8 +0,0 @@
-// Pregunta al usuario por su nombre
-const nombre = prompt("¿Cuál es tu nombre?");
-
-if (nombre !== null) {
-  console.log(`Hola, ${nombre}!`);
-} else {
-  console.log("No se ingresó ningún nombre.");
-}
